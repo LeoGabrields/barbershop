13fb5ea110285f2cccc8089b7f37a83aa98d7d24
Leandro Gabriel <leogabrields97@gmail.com>
2023-08-16 19:03:11 -0300

    added user login service provider

--- barbershop/lib/src/core/providers/application_providers.dart
+++ barbershop/lib/src/core/providers/application_providers.dart
@@ -1,7 +1,9 @@
-import 'package:barbershop/src/repositories/user/user_repository_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../repositories/user/user_repository.dart';
+import '../../repositories/user/user_repository_impl.dart';
+import '../../services/user_login/user_login_service.dart';
+import '../../services/user_login/user_login_service_impl.dart';
 import '../restClient/rest_client.dart';
 
 part 'application_providers.g.dart';
@@ -12,3 +14,7 @@ RestClient restClient(RestClientRef ref) => RestClient();
 @Riverpod(keepAlive: true)
 UserRepository userRepository(UserRepositoryRef ref) =>
     UserRepositoryImpl(restClient: ref.read(restClientProvider));
+
+@Riverpod(keepAlive: true)
+UserLoginService userLoginService(UserLoginServiceRef ref) =>
+    UserLoginServiceImpl(userRepository: ref.read(userRepositoryProvider));
