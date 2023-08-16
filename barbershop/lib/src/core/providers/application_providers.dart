d78307edeff631cd4e6ee1e14aa4c3496b7e7abd
Leandro Gabriel <leogabrields97@gmail.com>
2023-08-15 21:26:38 -0300

    create userRepository login

--- barbershop/lib/src/core/providers/application_providers.dart
+++ barbershop/lib/src/core/providers/application_providers.dart
@@ -1,8 +1,14 @@
+import 'package:barbershop/src/repositories/user/user_repository_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import '../../repositories/user/user_repository.dart';
 import '../restClient/rest_client.dart';
 
 part 'application_providers.g.dart';
 
 @Riverpod(keepAlive: true)
 RestClient restClient(RestClientRef ref) => RestClient();
+
+@Riverpod(keepAlive: true)
+UserRepository userRepository(UserRepositoryRef ref) =>
+    UserRepositoryImpl(restClient: ref.read(restClientProvider));
