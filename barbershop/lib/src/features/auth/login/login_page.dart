49fd7a656d6084895b8a95ea43a66c8e0dbb0dc2
Leandro Gabriel <leogabrields97@gmail.com>
2023-08-16 19:19:31 -0300

    implemented form and validation of the login page

--- barbershop/lib/src/features/auth/login/login_page.dart
+++ barbershop/lib/src/features/auth/login/login_page.dart
@@ -1,110 +1,140 @@
+import 'package:barbershop/src/core/ui/widgets/helpers/form_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:validatorless/validatorless.dart';
 
 import '../../../core/ui/constants.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  final emailEC = TextEditingController();
+  final passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    emailEC.dispose();
+    passwordEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: DecoratedBox(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(ImageConstants.backgroundChair),
-            opacity: 0.2,
-            fit: BoxFit.cover,
+      body: Form(
+        key: formKey,
+        child: DecoratedBox(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(ImageConstants.backgroundChair),
+              opacity: 0.2,
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: CustomScrollView(
-            slivers: [
-              SliverFillRemaining(
-                hasScrollBody: false,
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(ImageConstants.imageLogo),
-                        const SizedBox(
-                          height: 48,
-                        ),
-                        TextFormField(
-                          onTapOutside: (event) {
-                            FocusScope.of(context).unfocus();
-                          },
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: const InputDecoration(
-                            label: Text('E-mail'),
-                            hintText: 'E-mail',
-                            floatingLabelBehavior: FloatingLabelBehavior.never,
-                            labelStyle: TextStyle(color: Colors.black),
-                            hintStyle: TextStyle(color: Colors.black),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: CustomScrollView(
+              slivers: [
+                SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(ImageConstants.imageLogo),
+                          const SizedBox(
+                            height: 48,
                           ),
-                        ),
-                        const SizedBox(
-                          height: 24,
-                        ),
-                        TextFormField(
-                          obscureText: true,
-                          onTapOutside: (event) {
-                            FocusScope.of(context).unfocus();
-                          },
-                          decoration: const InputDecoration(
-                            label: Text('Senha'),
-                            hintText: 'Senha',
-                            floatingLabelBehavior: FloatingLabelBehavior.never,
-                            labelStyle: TextStyle(color: Colors.black),
-                            hintStyle: TextStyle(color: Colors.black),
+                          TextFormField(
+                            controller: emailEC,
+                            onTapOutside: (_) => unfocus(context),
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: const InputDecoration(
+                              label: Text('E-mail'),
+                              hintText: 'E-mail',
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.never,
+                              labelStyle: TextStyle(color: Colors.black),
+                              hintStyle: TextStyle(color: Colors.black),
+                            ),
+                            validator: Validatorless.multiple([
+                              Validatorless.required('E-mail obrigatório'),
+                              Validatorless.email('E-mail inválido'),
+                            ]),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        const Align(
-                          alignment: Alignment.centerLeft,
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          TextFormField(
+                            controller: passwordEC,
+                            onTapOutside: (_) => unfocus(context),
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              label: Text('Senha'),
+                              hintText: 'Senha',
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.never,
+                              labelStyle: TextStyle(color: Colors.black),
+                              hintStyle: TextStyle(color: Colors.black),
+                            ),
+                            validator: Validatorless.multiple([
+                              Validatorless.required('Senha obrigatória'),
+                              Validatorless.min(6,
+                                  'Senha deve conter pelo menos 6 caracteres'),
+                            ]),
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Esqueceu a senha?',
+                              style: TextStyle(
+                                color: ColorsConstants.brow,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size.fromHeight(56),
+                            ),
+                            onPressed: () {},
+                            child: const Text('ACESSAR'),
+                          )
+                        ],
+                      ),
+                      const Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: EdgeInsets.only(bottom: 40),
                           child: Text(
-                            'Esqueceu a senha?',
+                            'Criar conta',
                             style: TextStyle(
-                              color: ColorsConstants.brow,
-                              fontSize: 12,
+                              fontSize: 16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
                             ),
                           ),
                         ),
-                        const SizedBox(
-                          height: 24,
-                        ),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: const Size.fromHeight(56),
-                          ),
-                          onPressed: () {},
-                          child: const Text('ACESSAR'),
-                        )
-                      ],
-                    ),
-                    const Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Padding(
-                        padding: EdgeInsets.only(bottom: 40),
-                        child: Text(
-                          'Criar conta',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
