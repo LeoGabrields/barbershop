0539c56e52c741db67900953a0553989c8822365
Leandro Gabriel <leogabrields97@gmail.com>
2023-08-18 09:39:35 -0300

    userRepository register adm

--- barbershop/lib/src/repositories/user/user_repository_impl.dart
+++ barbershop/lib/src/repositories/user/user_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'dart:io';
 
+import 'package:barbershop/src/core/fp/nil.dart';
 import 'package:dio/dio.dart';
 
 import '../../core/exceptions/auth_exception.dart';
@@ -52,6 +53,25 @@ class UserRepositoryImpl implements UserRepository {
     } on ArgumentError catch (e, s) {
       log('Invalid Json', error: e, stackTrace: s);
       return Failure(RepositoryException(message: e.message));
+    }
+  }
+
+  @override
+  Future<Either<RepositoryException, Nil>> registerAdmin(
+      ({String email, String name, String password}) userData) async {
+    try {
+      await restClient.unAuth.post('/users', data: {
+        'name': userData.name,
+        'email': userData.email,
+        'password': userData.password,
+        'profile': 'ADM',
+      });
+      return Success(nil);
+    } on DioException catch (e, s) {
+      log('Erro ao registrar usuário', error: e, stackTrace: s);
+      return Failure(
+        RepositoryException(message: 'Erro ao registrar usuário admin'),
+      );
     }
   }
 }
